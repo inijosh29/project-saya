9282bd4cbab615133222c2473120c08b15eea9bb
inijosh29 <joshuabagus5@gmail.com>
2024-12-18 13:01:57 +0800

    first commit

---
+++ biodata.php
@@ -0,0 +1,19 @@
+<?php 
+    $nama = "budi";
+    $asal = "SMK AIRLANGGA";
+    $jur  = "Rekayasa Perangkat Lunak";
+    $tempat = "rumah";
+    $hobi = "main";
+    echo "= = = = = = = = = = = = = = = BIODATA = = = = = = = = = = = = = = =";
+    echo "<br>";
+    echo "<br>";
+    echo "nama saya adalah $nama";
+    echo "<br>";
+    echo "asal sekolah $asal";
+    echo "<br>";
+    echo "jurusan $jur";
+    echo "<br>";
+    echo "tempat asal $tempat";
+    echo "<br>";
+    echo "= = = = = = = = = = = = = = = END = = = = = = = = = = = = = = =";
+?>
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+    echo "Hello PHP!";
+    $names = "Rama";
+
+    echo "<br>";
+    echo $names;
+    echo "<br>";
+    echo "Hello my name is $names";
+?>
